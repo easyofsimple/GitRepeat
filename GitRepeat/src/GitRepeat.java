947bba120f961f20e80329e61d3d591dea60edf5
user name <user@email.com>
2014-05-27 11:29:33 -0400

    check in again after creating the scondBranchNonCloud.

--- GitRepeat/src/GitRepeat.java
+++ GitRepeat/src/GitRepeat.java
@@ -5,8 +5,9 @@ public class GitRepeat {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("first print out after comitting to remote github");
-
+		System.out.println("hello first print: first print out after comitting to remote github");
+		System.out.println("---------------------------------------------------");
+		System.out.println("this is another print out on secondBranchNonCloud");
 	}
 
 }
